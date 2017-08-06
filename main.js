7d08766997a9ff802a56c7fc6bee8a80cbca4e79
Adam Smith <hagansmith@gmail.com>
2017-08-06 07:17:30 -0500

    add js functionality, improved conversion, removed need for button press, add CSS styling for colors

--- main.js
+++ main.js
@@ -1,9 +1,19 @@
 //console.log('in main.js');
 var temperature = document.getElementById('temperature');
 
-// Get a reference to the button element in the DOM
-var button = document.getElementById("btnConvert");
-button.addEventListener("click", temp);
+//Get a reference to the button element in the DOM
+// var button = document.getElementById("btnConvert");
+// button.addEventListener("click", temp);
+// btnPress();
+// function btnPress() {
+// 	var button = document.getElementById("btnConvert");
+//   button.addEventListener("click", temp);
+// }
+
+// This allows the diplayed temp calculation to update after each number that is entered into the text field
+document.getElementById("temperature").addEventListener("keyup", temp);
+document.getElementById('rdoFah').addEventListener('click', temp);
+document.getElementById('rdoCel').addEventListener('click', temp);
 
 function temp() {
 	var getInput = parseInt(temperature.value);
@@ -22,50 +32,54 @@ function temp() {
 
 }
 
-
-
-//console.log("the ending temperatue is " + temperature + " and should not match the start");
-
-
 function toCelsius (celTemp) {
  //T(°C) = (T(°F) - 32) × 5/9
- var tempC = (celTemp - 32) * 1.8;
+ var tempC = (celTemp - 32) * (5/9);
  return tempC;
 }
 
 function toFahrenheit(fahTemp) {
 // //T(°F) = T(°C) × 9/5 + 32
-var tempF = (fahTemp * 1.8) + 32;
+var tempF = (fahTemp * (5/9)) + 32;
 return tempF;
 }
 
 function scale (conversion, t) {
-var level;	
-	if (t === "c") {
-		if (conversion > 32) {
+var level;
+var format;
+var start;	
+	if (t === "c") { 
+		var format = "°C";
+		var start = "°F"
+			if (conversion > 32) {
 			level = "high";
 		} else if (conversion < 0) {
 			level = "low";
 		} else {
 			level = "mid";
-		}
-
+		}	
 	} else {
-		if (conversion > 90) {
+		var format = "°F";
+		var start = "°C"
+			if (conversion > 90) {
 			level = "high";
 		} else if (conversion < 32) {
 			level = "low";
 		} else {
 			level = "mid";
 		}
+		
 	}
 
 var string = "";
 var container = document.getElementById('tempConverted');
-string += '<h3 id = "tempConverted" class =' + level + '> Your converted temp is ' + conversion + '</h3>' 
+if (!isNaN(conversion) === true) {
+string += '<h3 id = "tempConverted" class =' + level + '>' + temperature.value + start + ' is '  + conversion.toFixed(2) + format + '</h3>' 
+} else {
+ string += '<h3 id = "tempConverted" class ="black"> Please select a conversion and place a value in the box</h3>' 
+}
 
 container.innerHTML = string;
-
 }
 
  
